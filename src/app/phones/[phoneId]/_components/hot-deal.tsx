9d18f26918a3fbf3a3f80bb79320ab2e971a36ed
stunnerhash <utyadav07860@gmail.com>
2024-06-25 10:17:29 +0530

    hotdeal skeleton fixed

--- src/app/phones/[phoneId]/_components/hot-deal.tsx
+++ src/app/phones/[phoneId]/_components/hot-deal.tsx
@@ -25,7 +25,7 @@ export default async function HotDeal({ phoneId }: { phoneId: number }) {
 
 export function HotDealSkeleton(){
   return(
-    <div className="flex flex-col w-full group">
+    <div className="col-span-2 flex flex-col w-full group">
       <div className="p-4 transitiona-all duration-1000 opacity-90 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
         <p className="text-background text-xl font-bold ">Hot deal</p>
         <DealCardSkeleton/>
